60acf06afe8c632c42f510fbb27705082a98b9e8
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-10-08 03:44:44 +0200

    simple benchmarks

---
+++ rust/fast-algo/src/lib.rs
@@ -0,0 +1,118 @@
+#![allow(unused)]
+
+use std::collections::HashSet;
+
+pub const TEST: &'static str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+pub fn simple(input: &[u8]) -> Option<usize> {
+    input
+        .windows(14)
+        .position(|w| w.iter().collect::<HashSet<_>>().len() == 14)
+    // .map(|n| n + 14)
+}
+
+pub fn with_vec(input: &[u8]) -> Option<usize> {
+    input.windows(14).position(|w| {
+        let mut buf = Vec::with_capacity(14);
+        for c in w {
+            if buf.contains(c) {
+                return false;
+            }
+            buf.push(*c);
+        }
+        true
+    })
+}
+
+pub fn with_array(input: &[u8]) -> Option<usize> {
+    input.windows(14).position(|w| {
+        let mut buf = [0u8; 14];
+        for (idx, c) in w.iter().enumerate() {
+            for i in 0..idx {
+                if buf[i] == *c {
+                    return false;
+                }
+            }
+            buf[idx] = *c;
+        }
+        true
+    })
+}
+
+pub fn bitwise(input: &[u8]) -> Option<usize> {
+    input.windows(14).position(|w| {
+        let mut state = 0u32;
+        for c in w {
+            let prev = state;
+            state |= 1 << (c % 32);
+            if state == prev {
+                return false;
+            }
+        }
+        true
+    })
+}
+
+pub fn xor(input: &[u8]) -> Option<usize> {
+    let mut filter = 0u32;
+    input
+        .iter()
+        .take(14 - 1)
+        .for_each(|c| filter ^= 1 << (c % 32));
+    // println!("+ {filter:b}");
+
+    input.windows(14).position(|w| {
+        let first = w[0];
+        let last = w[w.len() - 1];
+        // println!("* {filter:b}");
+        filter ^= 1 << (last % 32);
+        // println!("- {filter:b}");
+        let res = filter.count_ones() == 14 as u32;
+        filter ^= 1 << (first % 32);
+        // println!("- {filter:b} {res}");
+        res
+    })
+}
+
+pub fn reverse(input: &[u8]) -> Option<usize> {
+    let mut idx = 0;
+    while let Some(slice) = input.get(idx..idx + 14) {
+        let mut state = 0u32;
+        // println!("* {state:0>27b}");
+
+        if let Some(pos) = slice.iter().rposition(|c| {
+            let bit_idx = c % 32;
+            let ret = state & (1 << bit_idx) != 0;
+            state |= 1 << bit_idx;
+            /* println!(
+                "- {state:0>27b} char={} bit={:<2} count={}",
+                *c as char,
+                bit_idx,
+                state.count_ones()
+            ); */
+            ret
+        }) {
+            idx += pos + 1;
+            // println!("> pos={pos} idx={idx}");
+        } else if state.count_ones() == 14 as u32 {
+            return Some(idx);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn main() {
+        assert_eq!(dbg!(simple(TEST.as_bytes())), Some(12));
+        assert_eq!(dbg!(with_vec(TEST.as_bytes())), Some(12));
+        assert_eq!(dbg!(with_array(TEST.as_bytes())), Some(12));
+        assert_eq!(dbg!(bitwise(TEST.as_bytes())), Some(12));
+        // assert_eq!(dbg!(xor(TEST.as_bytes())), Some(12));
+        assert_eq!(dbg!(reverse(TEST.as_bytes())), Some(12));
+    }
+}
