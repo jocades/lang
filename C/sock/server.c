dc095db4026a158c6faa02986993fd4eb0c02b42
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2025-01-10 20:49:01 +0100

    non blocking with threads

--- C/sock/server.c
+++ C/sock/server.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -7,19 +8,72 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8000
 #define BUF_SIZE 1024
+#define MAX_CONNECTIONS 2
 
 typedef struct {
   int sock;
+  struct sockaddr_in addr;
+} Server;
+
+typedef struct {
+  int sock;
+  struct sockaddr_in addr;
   char buf[BUF_SIZE];
 } Client;
 
-void* process(Client* client) {
+typedef struct {
+  int conn_count;
+  pthread_mutex_t mutex;
+} State;
+
+State state;
+
+void serve(Server* server, uint16_t port) {
+  server->sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (server->sock < 0) {
+    perror("failed to create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  int opt = 1;
+  int err =
+      setsockopt(server->sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  if (err != 0) {
+    perror("failed to set SO_REUSEADDR");
+    exit(EXIT_FAILURE);
+  }
+
+  server->addr.sin_family = AF_INET;
+  server->addr.sin_addr.s_addr = INADDR_ANY;
+  server->addr.sin_port = htons(port);
+  socklen_t addrlen = sizeof(server->addr);
+
+  err = bind(server->sock, (struct sockaddr*)&server->addr, addrlen);
+  if (err < 0) {
+    perror("failed to bind socket");
+    exit(EXIT_FAILURE);
+  }
+
+  err = listen(server->sock, MAX_CONNECTIONS);
+  if (err < 0) {
+    perror("failed to listen");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Listening @ localhost:%d\n", port);
+}
+
+void* process(void* arg) {
+  Client* client = (Client*)arg;
+
   while (true) {
     int n = read(client->sock, client->buf, BUF_SIZE);
     if (n <= 0) {
-      printf("Client closed connection\n");
+      printf(
+          "Client disconnected %s:%d\n", inet_ntoa(client->addr.sin_addr),
+          client->addr.sin_port
+      );
       break;
     }
 
@@ -29,82 +83,48 @@ void* process(Client* client) {
   }
 
   close(client->sock);
+  free(client);
 
   return NULL;
 }
 
-typedef struct {
-  int sock;
-  struct sockaddr_in addr;
-  socklen_t addrlen;
-} Server;
-
-void serve(Server* server) {
-  // Create socket file descriptor
-  server->sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (server->sock < 0) {
-    perror("failed to create socket");
-    exit(EXIT_FAILURE);
-  }
-
-  // Set socket options (on macOS cannot use bitwise OR to set socket options,
-  // hence we need 2 separate calls)
-  int opt = 1;
-  int err =
-      setsockopt(server->sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-  if (err != 0) {
-    perror("failed to set SO_REUSEADDR");
-    exit(EXIT_FAILURE);
-  }
-
-  // Configure address structure
-  server->addrlen = sizeof(server->addr);
-  server->addr.sin_family = AF_INET;
-  server->addr.sin_addr.s_addr = INADDR_ANY;
-  server->addr.sin_port = htons(PORT);
-
-  // Bind socket to port
-  err = bind(server->sock, (struct sockaddr*)&server->addr, server->addrlen);
-  if (err < 0) {
-    perror("failed to bind socket");
-    exit(EXIT_FAILURE);
-  }
-
-  // Listen for connections
-  err = listen(server->sock, 3);
-  if (err < 0) {
-    perror("failed to listen");
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Listening @ localhost:%d\n", PORT);
-}
-
-typedef struct {
-  Client* client;
-  size_t len;
-  size_t cap;
-} Clients;
-
-void push(Clients* clients, Client* client) {}
-
 int main() {
   Server server;
-  serve(&server);
+  serve(&server, 8000);
 
-  Client clients[2];
+  state.conn_count = 0;
+  pthread_mutex_init(&state.mutex, NULL);
 
   while (true) {
-    Client client = clients[0];
-    client.sock =
-        accept(server.sock, (struct sockaddr*)&server.addr, &server.addrlen);
-    if (client.sock < 0) {
-      perror("failed to accept");
-      exit(EXIT_FAILURE);
+    Client* client = malloc(sizeof(Client));
+    if (client == NULL) {
+      perror("failed to allocate client");
+      continue;
     }
 
-    printf("New client connected\n");
-    process(&client);
+    socklen_t addrlen = sizeof(client->addr);
+    client->sock =
+        accept(server.sock, (struct sockaddr*)&client->addr, &addrlen);
+
+    if (client->sock < 0) {
+      perror("failed to accept");
+      free(client);
+      continue;
+    }
+
+    printf(
+        "New client connected %s:%d\n", inet_ntoa(client->addr.sin_addr),
+        client->addr.sin_port
+    );
+
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, process, client) != 0) {
+      perror("failed to create thread");
+      close(client->sock);
+      free(client);
+      continue;
+    }
+    pthread_detach(thread);
   }
 
   close(server.sock);
